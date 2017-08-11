daee58102a73d356a1951162518d1fd4922bdc9e
Bhavik Naik <bhavik.tennis@gmail.com>
2017-08-10 23:41:06 -0400

    Updates to the test website

--- testWebsite/js/script.js
+++ testWebsite/js/script.js
@@ -1,32 +1,10 @@
-(function () {
-    if (window.localStorage) {
-        if (!localStorage.getItem('firstLoad')) {
-            localStorage['firstLoad'] = true;
-            window.location.reload();
-        } else
-            localStorage.removeItem('firstLoad');
-    }
-})();
 $(document).ready(function () {
-    $(window).scroll(function () {
-        var $scrollingDiv = $("#navbar");
-        $scrollingDiv.stop()
-        $scrollingDiv.css("background-color", (($(window).scrollTop() / $(document).height()) > 0.15) ? "#242629" : "");
-        $scrollingDiv.css("transition", (($(window).scrollTop() / $(document).height()) > 0.15) ? "5s ease;" : "");
-        $('.hideme').each(function (i) {
-            var bottom_of_object = $(this).offset().top;
-            var bottom_of_window = $(window).scrollTop() + $(window).height() + $(window).height();
-            if (bottom_of_window >= bottom_of_object) {
-                $(this).animate({
-                    'opacity': '1'
-                }, 2000);
-            }
-        });
-    });
-    /*$(window).scroll(function () {
-
-    });*/
+	$('.materialboxed').materialbox();
 });
-$(document).ready(function () {
 
-});
+function scrolldown() {
+	$('html,body').animate({
+			scrollTop: $("#content").offset().top - 60
+		},
+		'slow');
+}
